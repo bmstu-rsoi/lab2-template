a13945b5fe6cddc84e8605550a9f46e4b988bb61
migregal <devmigregal@mail.ru>
2023-10-02 01:21:56 +0300

    #1: add rating srv to docker compose

---
+++ v4/src/cmd/rating/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/migregal/bmstu-iu7-ds-lab2/rating"
+	"github.com/migregal/bmstu-iu7-ds-lab2/rating/config"
+)
+
+func main() {
+	lg := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		lg.Error("[startup] failed to init config", "err", err.Error())
+		os.Exit(1)
+	}
+
+	app, err := rating.New(lg, cfg)
+	if err != nil {
+		lg.Error("[startup] failed to init app", "err", err.Error())
+		os.Exit(1)
+	}
+
+	app.Run(lg)
+}
--- v4/src/rating/app.go
+++ v4/src/rating/app.go
@@ -1,4 +1,4 @@
-package reservation
+package rating
 
 import (
 	"fmt"
